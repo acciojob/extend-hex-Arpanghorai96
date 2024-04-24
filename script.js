0cff70a46a6c0812bf3562956a46f7f9977698ca
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-04-24 20:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const extendHex = (shortHex) => {
   // write your code here
 	let newhexcode="";
+	let r,g,b;
 	if(shortHex[0]=="#")
 	{
 		
-	let r=shortHex[1];
-	let g=shortHex[2];
-	let b=shortHex[3];
+	r=shortHex[1];
+	g=shortHex[2];
+	b=shortHex[3];
 	}
 	else
 	{	
-	let r=shortHex[0];
-	let g=shortHex[1];
-	let b=shortHex[2];
+	r=shortHex[0];
+	g=shortHex[1];
+	b=shortHex[2];
 	}
 	newhexcode=`#${r}${r}${g}${g}${b}${b}`;
 		return newhexcode;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
