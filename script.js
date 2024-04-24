40fef4f5ef08aa41bee69b341fa0d26c8793f416
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-04-24 20:39:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const extendHex = (shortHex) => {
   // write your code here
 	let newhexcode="";
+	if(shortHex[0]=="#")
+	{
+		
+	let r=shortHex[1];
+	let g=shortHex[2];
+	let b=shortHex[3];
+	}
+	else
+	{	
 	let r=shortHex[0];
 	let g=shortHex[1];
 	let b=shortHex[2];
+	}
 	let newhexcode=`#${r}${r}${g}${g}${b}${b}`;
 		return newhexcode;
 };
