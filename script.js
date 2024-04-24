a7044b52b6e636c263772d0bad68ad3911118112
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-04-24 20:36:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code here
 	let newhexcode="";
-	for(let i=0;i<shortHex.lenghtl;i++)
-		{
-			if(shortHex.charAt(0)=="#")
-			{
-				continue;
-			}
-			else
-			{
-				newhexcode=newhexcode+shortHex.charAt(i)+shortHex.charAt(i);
-			}
-		}
-	return "#"+newhexcode;
+	let r=shortHex[0];
+	let g=shortHex[1];
+	let b=shortHex[2];
+	let newhexcode=`#${r}${r}${g}${g}${b}${b}`;
+		return newhexcode;
 };
 
 // Do not change the code below.
