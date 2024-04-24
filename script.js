bdd4bbb93803a7f65b56a2aaffb93e8ae5cfd951
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-04-24 20:25:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let newhexcode="";
+	for(let i=0;i<shortHex.lenghtl;i++)
+		{
+			if(shortHex.charAt(0)=="#")
+			{
+				continue;
+			}
+			else
+			{
+				newhexcode=newhexcode+shortHex.charAt(i)+shortHex.charAt(i);
+			}
+		}
+	return "#"+newhexcode;
 };
 
 // Do not change the code below.
